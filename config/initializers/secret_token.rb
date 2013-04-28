57a3033b619e2ded453454b3f1631de5c3eb4cf3
Yena Chung <yenachung@gmail.com>
2013-04-28 17:29:45 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yenarails::Application.config.secret_token = 'd2e99f152f9d115bdfa4ddb533d030ab3c0d508b9525ce4e6034cf14ce02096f8e57c26c99bf3e86839ea27480ab305749860f07c0166319dbb48803c9851e31'
